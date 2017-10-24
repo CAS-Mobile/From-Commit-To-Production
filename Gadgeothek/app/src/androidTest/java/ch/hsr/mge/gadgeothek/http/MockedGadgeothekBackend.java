3ad2189cd4951f96df988c530fc8437b4b732f38
Yves Bonjour <yves.bonjour@ergon.ch>
2017-10-24 08:43:03 +0200

    Adds MockedGadgeothekBackend

---
+++ Gadgeothek/app/src/androidTest/java/ch/hsr/mge/gadgeothek/http/MockedGadgeothekBackend.java
@@ -0,0 +1,31 @@
+package ch.hsr.mge.gadgeothek.http;
+
+
+import static ch.hsr.mge.gadgeothek.http.Endpoint.get;
+import static ch.hsr.mge.gadgeothek.http.Endpoint.post;
+
+public class MockedGadgeothekBackend {
+    private final HttpProxy httpProxy;
+
+    public MockedGadgeothekBackend(HttpProxy httpProxy) {
+        this.httpProxy = httpProxy;
+    }
+
+    public void givenRegisterSuccessful() {
+        httpProxy.mockCall(post("/public/register"), "true");
+    }
+
+    public void givenLoginSuccessful(String customerId, String securityToken) {
+        String loginResponse = "{\"customerId\": \"" + customerId + "\", \"securityToken\": \"" + securityToken + "\"}";
+        httpProxy.mockCall(post("/public/login"), loginResponse);
+    }
+
+    public void givenEmptyLoans() {
+        httpProxy.mockCall(get("/public/loans"), "[]");
+    }
+
+    public void givenEmptyReservations() {
+        httpProxy.mockCall(get("/public/reservations"), "[]");
+    }
+
+}
