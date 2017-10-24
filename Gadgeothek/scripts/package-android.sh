302cbcaf638ec65490b353bb71efc98129486881
Yves Bonjour <yves.bonjour@ergon.ch>
2017-10-24 17:50:35 +0200

    Adds package script and includes it in pipeline

---
+++ Gadgeothek/scripts/package-android.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+root="${dir}/.."
+
+unsigned_apk="${root}/app/build/outputs/apk/app-full-release-unsigned.apk"
+unsigned_aligned_apk="${root}/app/build/outputs/apk/app-release-unsigned-aligned.apk"
+keystore="${HOME}/gadgeothek.keystore"
+keystore_pass="${HOME}/keystore-pass"
+
+(cd "${root}"; ./gradlew clean assembleRelease)
+
+apk_name="Gadgeothek"
+
+echo "Creating APK with name: ${apk_name}."
+
+signed_apk="${root}/app/build/outputs/apk/${apk_name}.apk"
+
+zipalign -v 4 "${unsigned_apk}" "${unsigned_aligned_apk}"
+
+apksigner sign --ks "${keystore}" --ks-pass file:"${keystore_pass}" --out "${signed_apk}" "${unsigned_aligned_apk}"
